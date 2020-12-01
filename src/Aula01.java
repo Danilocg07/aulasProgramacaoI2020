742261d7ad511b88b0a9a1b97726a8fb504bf2a3
Danilocg07 <danilo-c-g@hotmail.com>
2020-12-01 20:04:18 -0300

    Aulas de ProgramaçãoI UEG 2020-2

---
+++ src/Aula01.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Aula01 {
+    public static void main(String [] args){
+        JOptionPane.showMessageDialog(null,"Olá Mundo!");
+            System.out.println("Primeiro Código Java!");
+    }
+}
